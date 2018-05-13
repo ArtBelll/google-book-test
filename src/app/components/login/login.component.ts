83b292a6078721e1698ef3b7708fb0b1b65cd6c4
Artur Belogur <artur.belogur.korbit@gmail.com>
2018-05-13 22:12:36 +0200

    Deploy

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,24 +8,13 @@ import {LoginService} from '../../services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  readonly url = 'https://accounts.google.com/o/oauth2/v2/auth';
-  readonly client_id = '101136761905-kfc45eum300aueiqeakcq45qjnr5m95i.apps.googleusercontent.com';
-  readonly response_type = 'token';
-  readonly scope = 'https://www.googleapis.com/auth/books';
-  readonly redirect_uri = 'http://localhost:4200/save-token';
-
   loginUrl: String;
 
   constructor(private loginService: LoginService) {
   }
 
   ngOnInit() {
-    this.loginUrl = `${this.url}
-                    ?client_id=${this.client_id}
-                    &response_type=${this.response_type}
-                    &scope=${this.scope}
-                    &redirect_uri=${this.redirect_uri}`
-                    .replace(/ /g, '');
+    this.loginUrl = this.loginService.getAuthUrl();
   }
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
 import 'rxjs/add/operator/map';
+import {environment} from "../../environments/environment";
 
 @Injectable()
 export class LoginService {
 
-  constructor(private httpClient: HttpClient) { }
+  private readonly url = 'https://accounts.google.com/o/oauth2/v2/auth';
+  private readonly client_id = '101136761905-kfc45eum300aueiqeakcq45qjnr5m95i.apps.googleusercontent.com';
+  private readonly response_type = 'token';
+  private readonly scope = 'https://www.googleapis.com/auth/books';
+  private readonly redirect_uri = `${environment.url}/save-token`;
 
+  constructor() {
+  }
+
+  getAuthUrl(): string {
+    return `${this.url}
+                    ?client_id=${this.client_id}
+                    &response_type=${this.response_type}
+                    &scope=${this.scope}
+                    &redirect_uri=${this.redirect_uri}`
+      .replace(/ /g, '');
+  }
 }
